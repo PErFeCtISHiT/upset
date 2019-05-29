71af4faa687f217dbca23dfd235face7e7513961
WhiskyChoy <1261249659@qq.com>
2019-05-30 01:14:51 +0800

    test

--- upset.py
+++ upset.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
-from tensorflow import keras
+import numpy as np
+import keras
+import loader
 
 fashion_mnist = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = loader.load_data()
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
@@ -18,6 +20,9 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               metrics=['accuracy'])
 model.fit(train_images, train_labels, epochs=5)
 
+for layer in model.layers:
+    layer.trainable = False
+
 # 上面是评估图片的模型，下面是upset模型
 batch_size = 8
 
@@ -30,9 +35,13 @@ arg_s = 2
 arg_w = 0.1
 
 
+def flatten(images):
+    return np.array(images).reshape(len(images), -1)
+
+
 def get_weight(shape, lam):
     var = tf.Variable(tf.random_normal(shape), dtype=tf.float32)
-    tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(lam)(var))
+    # tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(lam)(var))
     return var
 
 
@@ -52,21 +61,17 @@ for i in range(1, num_layers):
 
 # x^
 new_image = tf.maximum(tf.minimum(arg_s * current_layer + train_y, 1), -1)
-
-
-lc = -tf.reduce_mean(train_x * tf.log(tf.clip_by_value(model.predict([new_image])[0])))
+lc = -tf.reduce_mean(train_x * tf.log(tf.clip_by_value(model.output, 1e-10, 1.0)))
 lf = 0.08 * tf.reduce_mean(tf.square(new_image - train_y))
 loss = lc + lf
 
-tf.add_to_collection('losses', loss)
+# tf.add_to_collection('losses', loss)
 
-total_loss = tf.add_n(tf.get_collection('losses'))
+# total_loss = tf.add_n(tf.get_collection('losses'))
 
 train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
 
 dataset_size = 50000
-
-Y = train_images
 
 with tf.Session() as session:
     init_op = tf.global_variables_initializer()
@@ -76,9 +81,13 @@ with tf.Session() as session:
         start = (i * batch_size) % dataset_size
         end = min(start + batch_size, dataset_size)
 
-        session.run(train_step, feed_dict={train_x: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], train_y: Y[start:end]})
+        session.run(train_step,
+                    feed_dict={train_x: np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]).repeat(batch_size, axis=0),
+                               train_y: flatten(train_images[start:end])})
 
         if i % 1000 == 0:
-            total_cross_entropy = session.run(loss, feed_dict={train_x: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], train_y: Y})
+            total_cross_entropy = session.run(loss, feed_dict={
+                train_x: np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]).repeat(batch_size, axis=0),
+                train_y: flatten(train_images)})
 
             print("After %d training step(s), cross entropy on all data is %g" % (i, total_cross_entropy))
