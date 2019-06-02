0cc79570e60312117d1399ae9eea82f4664bf9c5
WhiskyChoy <1261249659@qq.com>
2019-06-02 20:19:51 +0800

    Merge branch 'master' of https://github.com/PErFeCtISHiT/upset
    # Conflicts:
    #	new_upset.py

--- new_upset.py
+++ new_upset.py
@@ -72,9 +72,12 @@ w1_n = tf.convert_to_tensor(np.load('w1.npy'))
 w2_n = tf.convert_to_tensor(np.load('w2.npy'))
 
 model = get_model(w1_n, w2_n, new_image)
-model = tf.nn.softmax(model)
 
-lc = -tf.reduce_mean(train_x * tf.log(tf.clip_by_value(model, 1e-10, 1.0)))
+# model = tf.nn.softmax(model)
+# lc = -tf.reduce_mean(train_x * tf.log(tf.clip_by_value(model, 1e-10, 1.0)))
+
+lc = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits_v2(labels=train_x, logits=model))
 lf = arg_w * tf.reduce_mean(tf.square(new_image - train_y))
 loss = lc + lf
 # tf.add_to_collection('losses', loss)
