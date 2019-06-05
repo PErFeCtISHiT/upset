f40ffae2b8326a4d196ea039062da5df284f285b
LAPTOP-PA95VDSS\Admin <1261249659@qq.com>
2019-06-05 09:15:49 +0800

    changes

--- attack/load_target_model.py
+++ attack/load_target_model.py
@@ -1,9 +1,12 @@
-import keras
 import tensorflow as tf
 from keras.models import Model
 from keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten
+import os
 
 weight_path = '../model_data/my_model_weights.h5'
+if not os.path.exists(weight_path):
+    print('权重路径', weight_path, '不存在!')
+    exit(0)
 
 
 def get_model_output(input_tensor):
@@ -25,7 +28,8 @@ def get_model_output(input_tensor):
         layer.trainable = False
     return model.output
 
-def test:
+
+def test():
     test_tensor = tf.zeros([10, 28, 28, 1])
     output = get_model_output(test_tensor)
     sess = tf.Session()
